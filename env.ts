a8530d3bc88bbe891d9a066bf9964152b9774120
samshara <sameerai2736@gmail.com>
2024-03-06 16:27:13 +0545

    fix: upgrade CI

--- env.ts
+++ env.ts
@@ -1,5 +1,5 @@
 import { defineConfig, Schema } from '@julr/vite-plugin-validate-env';
 
 export default defineConfig({
-    APP_TITLE: Schema.string().optional(),
+    APP_TITLE: Schema.string.optional(),
 })
