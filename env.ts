ffe0425b54f0d6e722b309d11e0b72e172504ac5
samshara <sameerai2736@gmail.com>
2024-03-06 16:10:18 +0545

    feat: upgrade to Vite 5

--- env.ts
+++ env.ts
@@ -1,5 +1,5 @@
 import { defineConfig, Schema } from '@julr/vite-plugin-validate-env';
 
 export default defineConfig({
-    APP_TITLE: Schema.string(),
+    APP_TITLE: Schema.string().optional(),
 })
